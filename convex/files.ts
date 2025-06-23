bd6544b1fc653f8d7ea06e0da5b5c52937cbab5c
Evan Haba <hippoguy101@gmail.com>
2025-06-23 14:08:28 -0700

    authorization based on org id and tokenIdentifier

--- convex/files.ts
+++ convex/files.ts
@@ -1,29 +1,74 @@
 import { ConvexError, v } from "convex/values";
-import { mutation, query } from "./_generated/server";
+import { mutation, MutationCtx, query, QueryCtx } from "./_generated/server";
+import { getUser } from "./users";
+
+async function hasAccessToOrg(
+    ctx: QueryCtx | MutationCtx, 
+    tokenIdentifier: string, 
+    orgId: string
+) {
+    const user = await getUser(ctx, tokenIdentifier);
+
+    const hasAccess = 
+    user.orgIds.includes(orgId) || 
+    user.tokenIdentifier.includes(orgId);
+
+    return hasAccess;
+}
 
 export const createFile = mutation({
     args: {
         name: v.string(),
+        orgId: v.string(),
     },
     async handler(ctx, args) {
         const identity = await ctx.auth.getUserIdentity();
+
         if (!identity) {
             throw new ConvexError("you must be logged in to upload a file");
         }
+
+        const hasAccess = await hasAccessToOrg(
+            ctx, 
+            identity.tokenIdentifier, 
+            args.orgId
+        );
+
+        if (!hasAccess) {
+            throw new ConvexError("you do not have access to this org");
+        }
+
         await ctx.db.insert("files", {
             name: args.name,
+            orgId: args.orgId,
         });
     },
 });
 
 export const getFiles = query({
-    args: {},
+    args: {
+        orgId: v.string(),
+    },
     async handler(ctx, args) {
         const identity = await ctx.auth.getUserIdentity();
 
         if (!identity) {
             return [];
         }
-        return ctx.db.query("files").collect();
+
+        const hasAccess = await hasAccessToOrg(
+            ctx, 
+            identity.tokenIdentifier, 
+            args.orgId
+        );  
+
+        if (!hasAccess) {
+            return [];
+        }
+
+        return ctx.db
+            .query("files")
+            .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
+            .collect();
     }
 })
